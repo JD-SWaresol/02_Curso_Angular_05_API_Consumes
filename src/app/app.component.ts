81b73ef354ab6a19e763ba3a866e5138e19873b3
Juan Delgado <juan.delgado@smartwaresol.com>
2024-11-28 11:55:57 -0600

    WIP_04: Configuracion de Componente App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductComponent } from "./components/product/product.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  // Aqui nosotros podemos importar directamente 'PorductComponent' sin necesidad de importarlo a 'app.module.ts' ya que 'app.component.ts' y 'product.component.ts' 
+  // estan en un standalone de tipo true
+  imports: [RouterOutlet, ProductComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
