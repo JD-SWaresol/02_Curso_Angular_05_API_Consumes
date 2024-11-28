7879f3d6de83efe887d7393e9c0b776488ea3981
Juan Delgado <juan.delgado@smartwaresol.com>
2024-11-28 13:27:51 -0600

    WIP_07: Configuraciones para Componentes y Servicios de Product

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-product',
@@ -7,6 +8,28 @@ import { Component } from '@angular/core';
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss'
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit{
+
+  // Dentro del Constructor inyectamos el servicio
+  constructor(private productService: ProductService){}
+
+  ngOnInit(): void {
+    
+  }
+
+  // Generamos el metodo para traer todos los productos
+  getProducts(){
+    this.productService.getProducts().subscribe({
+      // Aqui ocurren dos eventos: next(para capturar la informacion) y error (en caso de que se muestre algun error)
+
+      next: (result) => {
+
+      },
+      error: (err) => {
+        console.log(err);
+      }
+
+    });
+  }
 
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  
+  /* Creamos un metodo que nos permita obtener todos los productos guardados 
+      1.- Primero obtenemos la URL del recurso que deseamos consumir
+      2.- Generamo el metodo que retorne los productos 'getProduct()'
+          -> El metodo es de tipo 'Observable' de tipo <any> para no retornar un modelo
+          -> Retorna una consulta en base a la 'API_URL' y trae un resultado que  lo asigmna a 'res'.
+  */
+  API_URL: string = 'http://localhost:4200/assets/data/productos.json';
+
+  constructor(private httpClient: HttpClient) { 
+    /* 
+      Como ya fue definido dentro de 'app.config.ts' como un provider, el constructor ya puede identificar 'HttpClient' dentro del servicio
+    */
+
+
+   }
+
+   getProducts(): Observable<any>{
+      return this.httpClient.get(this.API_URL).pipe(res => res);
+   }
+
+  
+
 }
