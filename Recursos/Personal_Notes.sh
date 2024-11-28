c65b38981c0b954a6c9ff558a24637d5e61743f7
Juan Delgado <juan.delgado@smartwaresol.com>
2024-11-28 10:50:17 -0600

    WIP_01: Primeras Observaciones

---
+++ Recursos/Personal_Notes.sh
@@ -0,0 +1,12 @@
+////// Observaciones
+
++ A difrerencia de las versiones anteriores de Angular 17, podemos observar que angular nos proporciona dos archivos adicionales, los cuales son:
+
+    - app.config.ts : 
+
+    - app.routes.ts : Estaba presente en versiones anteriores pero se usaba como un modulo.
+
+
+~/src/main.ts
+
++ Archivo para mapear que componente se va a inicializar en la aplicacion. (Se hace mas directo)
