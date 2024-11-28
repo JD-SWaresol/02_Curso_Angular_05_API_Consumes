aa24f29d090ebbaa8563acb9d3829cdbb4352fbc
Juan Delgado <juan.delgado@smartwaresol.com>
2024-11-28 11:55:08 -0600

    WIP_03: Observaciones de configuracion de Componente y Servicio creados

--- Recursos/Personal_Notes.sh
+++ Recursos/Personal_Notes.sh
@@ -10,3 +10,16 @@
 ~/src/main.ts
 
 + Archivo para mapear que componente se va a inicializar en la aplicacion. (Se hace mas directo)
+
+
+~/src/app.component.ts
+
++ Angular 17 tiene un ' standalone : true ' ya que con esto ya no seria necesario inyectar los components creados dentro de 'app.module.ts'
+
+    import { Component } from '@angular/core';
+    import { RouterOutlet } from '@angular/router';
+
+    @Component({
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet],
