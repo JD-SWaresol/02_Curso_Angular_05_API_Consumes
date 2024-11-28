6d3d1e6356b75229eba0464908248da6ac864ffd
Juan Delgado <juan.delgado@smartwaresol.com>
2024-11-28 13:40:37 -0600

    WIP_09: Preparando variables para renderizacion

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product.service';
+import { ProductInterface } from '../../interfaces/product.interface';
 
 @Component({
   selector: 'app-product',
@@ -10,11 +11,14 @@ import { ProductService } from '../../services/product.service';
 })
 export class ProductComponent implements OnInit{
 
+  // Esta variable se va a usar para la renderizacion de los campos de una manera mas limpia. Inicializa en Blanco
+  productList: ProductInterface[]=[];
+
   // Dentro del Constructor inyectamos el servicio
   constructor(private productService: ProductService){}
 
   ngOnInit(): void {
-    
+    this.getProducts();
   }
 
   // Generamos el metodo para traer todos los productos
@@ -23,7 +27,7 @@ export class ProductComponent implements OnInit{
       // Aqui ocurren dos eventos: next(para capturar la informacion) y error (en caso de que se muestre algun error)
 
       next: (result) => {
-
+        this.productList = result.productos;
       },
       error: (err) => {
         console.log(err);
