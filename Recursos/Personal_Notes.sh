0f5c8340ddfed122c4ea85e104a7093185465438
Juan Delgado <juan.delgado@smartwaresol.com>
2024-11-28 13:28:45 -0600

    WIP_08: Actualizacion de Notas y Observaciones

--- Recursos/Personal_Notes.sh
+++ Recursos/Personal_Notes.sh
@@ -23,3 +23,20 @@
     selector: 'app-root',
     standalone: true,
     imports: [RouterOutlet],
+
+
++ HttpClientModule : Es usado para consumir servicios, esta presente para inyectores multiples. Sin embargo el comportamiento de los interceptores NO esta bien definido, 
+    depende de opciones exactas y de la orden de provider/import
+
++ provideHttpClient: Es usado para configuraciones multiyector, pues tiene un comportamiento mas estable.
+
+
++ Observables: Se usan como una interface para el manejo de una variedad comun de operaciones asincornas. Ejemplo:
+    * Los modulos HTTP usan 'Observables' para el manejo de peticiones y respuestas AJAX-
+    * Los modulos Router y FORMS usan 'Observables' para escuhar y responder los eventos de entradas de usuario.
+
+
+
+~/src/interfaces/product.interface.ts
+
++ Se usa para renderizar la información de los productos de una manera mas limpia
